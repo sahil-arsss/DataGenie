c80594252a23e9d634bec038259eef16ca8c78c6
sahil-arsss <sahilchaudhary@gmail.com>
2025-08-05 11:55:30 +0530

    changed variable

--- db/mysql.js
+++ db/mysql.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2";
-
+import dotenv from "dotenv";
+dotenv.config();
 export const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "23120041",
-  database: "SampleDB",
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE_NAME,
    multipleStatements: true,
 });
 
